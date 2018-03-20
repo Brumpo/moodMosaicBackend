29e0f0de769e29981140671b04d18ccbee0adf7e
Brent Schroder <schroder.brent@gmail.com>
2018-03-20 10:08:18 -0600

    fixed the seeds

--- migrations/seeds/01_users.js
+++ migrations/seeds/01_users.js
@@ -4,21 +4,21 @@ exports.seed = function(knex, Promise) {
   return knex('users').insert([
     {
     id: 1,
-    fname: 'sao',
-    lname: 'sao',
-    email: 'sao',
+    fname: 'Brent',
+    lname: 'Schroder',
+    email: 'schroder.brent@gmail.com',
     password: '$2b$12$D.UkGKlBy72ZqNz853./1e4bZTsCl/bfNmS8zahoeUEoxPpYvVwHa',
-    key1: 'feeling',
-    key2: 'love',
-    key3: 'religion',
-    key4: 'preaching',
-    key5: 'swearing',
-    key6: 'flavor flute'
+    key1: 'Hapiness',
+    key2: 'Anxiety',
+    key3: 'Irritability',
+    key4: 'Confidence',
+    key5: 'Sleep',
+    key6: 'Daily Meditation'
     },{
     id: 2,
     fname: 'Brent',
     lname: 'Schroder',
-    email: 'schroder.brent@gmail.com',
+    email: 'schroder.burnt@gmail.com',
     password: '$2b$12$FGQNmbaOgWle6CplOI1h4efvbPtOWJEJjgqMRHmrAWrdfvbVEvRDy'
     },{
     id: 3,
